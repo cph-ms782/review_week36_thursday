141a9f1e553fc6eba4b5b1dca818d9dedd523eda
Martin Sander-Thomsen <cph-ms782@cphbusiness.dk>
2019-09-05 17:53:37 +0200

    update

--- review_week36_thursday/src/main/webapp/EventBubbling.js
+++ review_week36_thursday/src/main/webapp/EventBubbling.js
@@ -6,15 +6,17 @@
 document.getElementById("id1").onclick = clickID1;
 document.getElementById("id2").onclick = clickID2;
 
-function  clickID1(){
+function  clickID1() {
 
-    document.getElementById("id1").innerHTML = "Hi from id1OfTheDiv";
+//    document.getElementById("id1").innerHTML = "Hi from id1OfTheDiv";
+    document.querySelector("#pTag").innerHTML = "Hi from id1OfTheDiv";
 
 }
 
-function  clickID2(){
+function  clickID2() {
 
-    document.getElementById("id2").innerHTML = "Hi from id2OfTheDiv";
+//    document.getElementById("id2").innerHTML = "Hi from id2OfTheDiv";
+    document.getElementById("pTag").innerHTML = "Hi from id2OfTheDiv";
 
 }
 
@@ -23,8 +25,19 @@ function  clickID2(){
  */
 document.getElementById("outer").onclick = clickMe;
 
-function clickMe(e){
-    console.log(e.target);
-    e.target.innerHTML = "Hi from "+e.target.id+"ofTheDiv";
-    e.target.className ="";
+function clickMe(e) {
+    //this id value - "this" er det element hvor eventlisteneren er blevet aktiveret
+    console.log(this.id);
+
+    //target id value - "target" er det element man trykker på 
+    console.log(e.target.id);
+
+    /**
+     * c.
+     */
+//    e.target.innerHTML = "Hi from " + e.target.id + "ofTheDiv";
+//    this.innerHTML = "Hi from " + e.target.id + "ofTheDiv";
+    document.querySelector("#pTag").innerHTML = e.target.id;
+
+
 }
