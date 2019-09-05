e8123205f32418f5e764ed931da9435a1b148cad
Martin Sander-Thomsen <cph-ms782@cphbusiness.dk>
2019-09-05 22:56:49 +0200

    ex update

--- review_week36_thursday/src/main/webapp/Fetch.js
+++ review_week36_thursday/src/main/webapp/Fetch.js
@@ -1,7 +1,45 @@
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Dynamic UI manipulation using data obtained via fetch
  */
+
+
+
+document.querySelector("#getUser").onclick = getUser;
+document.querySelector("#getAllUsers").onclick = getAllUsers;
+console.log("Logger");
+function getUser(e) {
+    e.preventDefault();
+    var ID = document.querySelector("#userID").value;
+    let url = "https://jsonplaceholder.typicode.com/users/" + ID;
+    console.log(url);
+    fetch(url)
+            .then(res => res.json()) //in flow1, just do it
+            .then(data => {
+                // Inside this callback, and only here, the response data is available
+//                console.log("data", data);
+                var tags = [];
+                Object.keys(data).forEach(function (key) {
+                    console.log(key + ":" + data[key]);
+                    tags.push(key + ":" + data[key]);
+                });
+                document.querySelector("#users").innerHTML = tags.join("<br />");
+            });
+}
+
+function getAllUsers(e) {
+    e.preventDefault();
+    let url = "https://jsonplaceholder.typicode.com/users";
+    console.log(url);
+    fetch(url)
+            .then(res => res.json()) //in flow1, just do it
+            .then(data => {
+                // Inside this callback, and only here, the response data is available
+                console.log("data", data);
+                /* data now contains the response, converted to JavaScript
+                 Observe the output from the log-output above
+                 Now, just build your DOM changes using the data*/
+            })
+}
+
 
 
